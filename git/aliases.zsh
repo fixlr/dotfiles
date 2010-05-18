6573bd5d80d8f0f38ce1d09fa7186b31595106de
Nathan Fixler <nathan@fixler.org>
2010-05-18 16:24:14 -0400

    Reordered the glog data so I can read it all on longer commit messages

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,5 +1,5 @@
 alias gl='git pull --rebase --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %C(yellow)%d%Creset %s - %Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
