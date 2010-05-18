ba6bca4cb7864a47ffadc33959284dc0c00e1f53
Nathan Fixler <nathan@fixler.org>
2010-05-18 16:24:44 -0400

    Cucumber is being stupid

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -10,3 +10,4 @@ alias scaffold='script/generate nifty_scaffold'
 alias migrate='rake db:migrate db:test:clone'
 alias rcov='rake rcov:all'
 alias s="ps aux | grep \"[r]uby\" | grep script/server || echo \"You're not running any, dawg.\""
+alias cuke='script/cucumber'
