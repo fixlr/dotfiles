70de58f3087e7e0cbfdebdf38f31c178630b1bfa
Nathan Fixler <nathan@fixler.org>
2011-02-01 23:19:35 -0500

    Parse server names from cnames

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -5,6 +5,12 @@ function cd() {
   ls
 }
 
+# so many SUNY libraries, so little time...
+function suny() {
+  dig $1.sunyconnect.suny.edu | \
+  egrep -o -m 1 '(saranac|delaware|seneca)\.sunyconnect\.suny\.edu'
+}
+
 # todo.sh finger savers
 alias t='todo.sh'
 alias tp='todo.sh -+ ls'
