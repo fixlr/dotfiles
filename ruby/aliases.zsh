763da8ead29ef7cf9779d7c337324fb650ebf5a0
Nathan Fixler <nathan@fixler.org>
2011-06-28 11:58:39 -0400

    Ruby/rails alias updates for 3.x

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,13 +1,8 @@
 alias gems='cd `rvm gemdir`'
 
-alias sc='script/console'
-alias ss='script/server -p `available_rails_port`'
-alias sg='script/generate'
-alias sd='script/destroy'
-alias a='autotest -rails'
+alias sc='bundle exec rails console'
+alias ss='bundle exec rails server'
+alias sg='bundle exec rails generate'
+alias sd='bundle exec rails destroy'
 alias tlog='tail -f log/development.log'
-alias scaffold='script/generate nifty_scaffold'
-alias migrate='rake db:migrate db:test:clone'
-alias rcov='rake rcov:all'
-alias s="ps aux | grep \"[r]uby\" | grep script/server || echo \"You're not running any, dawg.\""
-alias cuke='script/cucumber'
+alias migrate='bundle exec rake db:migrate db:test:clone'
