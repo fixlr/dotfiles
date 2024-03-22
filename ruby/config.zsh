14bc03f20a7b5cbe3bc0bcc282689f6502ad8366
Nathan Fixler <nathan@fixler.org>
2024-03-22 11:37:05 -0700

    Add solargraph config for Zed
    zed.dev supports solargraph's as an LSP for Ruby. This config sets some
    sensible defaults.

--- ruby/config.zsh
+++ ruby/config.zsh
@@ -2,3 +2,4 @@ export DISABLE_PRY_RAILS=true
 export DISABLE_SPRING=true
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 export RUBY_YJIT_ENABLE=1
+export SOLARGRAPH_GLOBAL_CONFIG="${HOME}/.solargraph_config.yml"
