81ec138a83d86255246218c9d25cfaeec9227ad9
Nathan Fixler <nathan@fixler.org>
2024-05-06 11:41:29 -0700

    Avoid issue with suspending xargs
    Backgrounding `xargs` + `vim` doesn't always work, even with the `-o`.
    I suspect that there's a way to make this work better, but avoiding the
    issue entirely by not using `xargs` also seems like a good option.

--- ag/aliases.zsh
+++ ag/aliases.zsh
@@ -1,6 +1,6 @@
 # Search for a list of files that match a pattern and open them in vim.
 function agvim() {
-  ag -l $* | xargs -o vim
+  vim $(ag -l0 $*)
 }
 
 alias ag='ag --path-to-ignore ~/.agignore'
