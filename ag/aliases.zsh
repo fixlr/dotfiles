13fe4a67d1f9d33e4da153fcf593eb624d450f84
Nathan Fixler <nathan@fixler.org>
2024-05-06 11:39:19 -0700

    Relocate agvim definition
    Does this belong in the vim or ag directory? Who can say...

--- ag/aliases.zsh
+++ ag/aliases.zsh
@@ -1,1 +1,6 @@
+# Search for a list of files that match a pattern and open them in vim.
+function agvim() {
+  ag -l $* | xargs -o vim
+}
+
 alias ag='ag --path-to-ignore ~/.agignore'
--- vim/aliases.zsh
+++
@@ -1,4 +0,0 @@
-# Search for a list of files that match a pattern and open them in vim.
-function agvim() {
-  ag -l $* | xargs -o vim
-}
